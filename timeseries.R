b14531f4ae9ba5069de6d93e8de5d96adb7c878c
TaylorFergML <159005218+TaylorFergML@users.noreply.github.com>
2024-05-16 01:45:34 -0700

    new forecast

--- timeseries.R
+++ timeseries.R
@@ -22,15 +22,8 @@ distinct(kingcountysales)
 # selecting variables and converting date
 
 king_time <- kingcountysales %>% 
-                select(sale_date, sale_price, city) %>% 
-                mutate(date = mdy(sale_date))
-
-# splitting date into month and year, filtering variables
-
-king_time <- king_time %>% 
-  mutate(sale_month = month(date)) %>% 
-  mutate(sale_year = year(date)) %>% 
-  select(sale_month, sale_year, sale_price, city)
+  select(sale_date, sale_price, city) %>% 
+  mutate(date = mdy(sale_date))
 
 # converting cities to factors
 
@@ -42,25 +35,153 @@ king_time$city <- as.factor(king_time$city)
 
 sum(is.na(king_time))
 
-# calculating median home sale by month and year
+# Calculating median by month
 
-monthly_median <- king_time %>%
-  group_by(sale_year, sale_month) %>%
-  summarise(median_sale = median(sale_price))
+install.packages("timetk")
 
-# Removing data near the great recession
-monthly_median <- monthly_median %>% filter(sale_year > 2013)
+library(timetk)
 
-# reshaping dataframe
+king_monthly <- 
+  king_time %>% 
+  summarize_by_time(.date_var = date,
+                    .by = "month",
+                    median_sale = median(sale_price, na.rm = TRUE))
 
-wide_monthly_median <- monthly_median %>% 
-  pivot_wider(names_from = sale_month, values_from = median_sale)
+head(king_monthly, 12)
 
-library(zoo)
-library(ggplot2)
-autoplot(zoo(t(monthly_median$median_sale)), facets = NULL)
+# Plotting monthly median sale price
+
+plot_time_series(king_monthly, 
+                 .date_var = date, 
+                 .value = median_sale,
+                 .interactive = TRUE,
+                 .x_lab = "Monthy Data",
+                 .y_lab = "Median Sale price")
+
+# Plotting monthly sales to find best months to buy or sell homes in king county
+
+plot_seasonal_diagnostics(king_monthly, .date_var = date, .value = median_sale)
+
+# Best month to sell by median home price is June
+
+# Best month to buy is January
+
+# Removing data near housing crisis to see if it changes monthly medians
+
+king_monthly_post2012 <- 
+  king_monthly %>% 
+  filter_by_time(.date_var = date, 
+                 .start_date = "2013",
+                 .end_date = "2024")
+
+glimpse(king_monthly_post2012, 12)
+
+plot_seasonal_diagnostics(king_monthly_post2012, .date_var = date, .value = median_sale)
+
+# This has similar results with June still being the best month to sell
+# but December edging out January as the best month to buy.
 
 
+# Building a simple univariant forecast model
+# -------------------------------------------
 
-glimpse(king_time)                
-    
+# Creating testing and training split
+
+library(rsample)
+
+king_monthly_split <- initial_time_split(king_monthly_post2012, prop = 130/136)
+
+king_training <- training(king_monthly_split)
+king_testing <- testing(king_monthly_split)
+
+# converting data to a tibble and setting index as date
+
+install.packages("fable")
+library(fable)
+library(tsibble)
+
+king_training <- 
+  king_training %>% 
+  mutate(date = yearmonth(date)) %>% 
+  as_tsibble(index = date)
+
+king_testing <- 
+  king_testing %>% 
+  mutate(date = yearmonth(date)) %>% 
+  as_tsibble(index = date)
+
+king_monthly_post2012 <- 
+  king_monthly_post2012 %>% 
+  mutate(date = yearmonth(date)) %>% 
+  as_tsibble(index = date)
+
+# Train the models
+
+install.packages("feasts")
+
+library(feasts)
+
+king_fit <- 
+  king_training %>% 
+  model(stepwise = ARIMA(median_sale),
+        search = ARIMA(median_sale, stepwise=FALSE))
+
+# Viewing model
+tidy(king_fit)
+
+king_fit %>% 
+  accuracy() %>% 
+  arrange(MAPE)
+
+# Checking forecast of training data vs full data set
+
+king_fit %>% 
+  forecast(h = "6 months") %>% 
+  autoplot(king_monthly_post2012)
+
+# final fit on full data set
+
+king_final_fit <- 
+  king_monthly_post2012 %>% 
+  model(stepwise = ARIMA(median_sale),
+        search = ARIMA(median_sale, stepwise=FALSE))
+
+# Fitting final model on full data set
+
+king_final_fit %>% 
+  forecast(h = "12 months") %>% 
+  autoplot(king_monthly_post2012) +
+  labs(x = "Month", y = "median home price")
+
+king_final_fit %>% 
+  forecast(h = "12 months") %>% 
+  autoplot() +
+  labs(x = "Month", y = "median home price")
+
+king_monthly_t <- 
+  king_monthly %>% 
+  mutate(date = yearmonth(date)) %>% 
+  as_tsibble(index = date)
+
+# The data ends in December of 2023, but referencing median sales in recent
+# we can see the forecast starts to get to high. I added back in the housing 
+# crisis data and retrained.
+
+king_final_fit_full <- 
+  king_monthly_t %>% 
+  model(stepwise = ARIMA(median_sale),
+        search = ARIMA(median_sale, stepwise=FALSE))
+
+king_final_fit_full %>% 
+  forecast(h = "12 months") %>% 
+  autoplot(king_monthly_t) +
+  labs(x = "Month", y = "median home price")
+
+king_final_fit_full %>% 
+  forecast(h = "12 months") %>% 
+  autoplot() +
+  labs(x = "Month", y = "median home price")
+
+# Adding the full time series makes the model more conservative and makes the 
+# forecast more accurate.
+
